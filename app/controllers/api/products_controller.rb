b3d66b98749897fceb6d7094c0eb35ce15a2be4a
dani <dani.zaghian@gmail.com>
2019-01-17 12:07:07 -0800

    adds mini capstone with all and individual routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,18 @@
 class Api::ProductsController < ApplicationController
+
+	def all_products_method
+		@products = Product.all #array of hashes
+		render 'all_products.json.jbuilder'
+	end
+
+	def first_product_method
+		@product = Product.first
+		render 'first_product.json.jbuilder'
+	end
+
+	def second_product_method
+		@product = Product.second
+		render 'second_product.json.jbuilder'
+	end
+
 end
