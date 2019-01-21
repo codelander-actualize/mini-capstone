f347c649e3bc03b78ccfdab6f0bd9562baef1f70
dani <dani.zaghian@gmail.com>
2019-01-21 12:16:59 -0800

    adds restful routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,24 +1,42 @@
 class Api::ProductsController < ApplicationController
 
-	def all_products_method
-		@products = Product.all #array of hashes
-		render 'all_products.json.jbuilder'
-	end
+	def index
+	    @products = Product.all
+	    render 'index.json.jbuilder'
+	  end
 
-	def first_product_method
-		@product = Product.first #single product hash
-		render 'first_product.json.jbuilder'
-	end
+	  def create
+	    @product = Product.new(
+	     	name: params[:name],
+	     	price: params[:price],
+	     	description: params[:description],
+	     	image_url: params[:image_url]
+	    )
+	    @product.save
+	    render 'show.json.jbuilder'
+	  end
 
-	def second_product_method
-		@product = Product.second
-		render 'second_product.json.jbuilder'
-	end
+	  def show
+	    @product = Product.find(params[:id])
+	    render 'show.json.jbuilder'
+	  end
 
-	def one_product
-		product_id = params[:id]
-		@product = Product.find_by(id: product_id) #single product hash
-		render 'first_product.json.jbuilder'
-	end
+	  def update
+	    @product = Product.find(params[:id])
+	    
+	    @product.name = params[:name] || @product.name
+	    @product.price = params[:price] || @product.price
+	    @product.description = params[:description] || @product.description
+	    @product.image_url = params[:image_url] || @product.image_url
+
+	    @product.save
+	    render 'show.json.jbuilder'
+	  end
+
+	  def destroy
+	    @product = Product.find(params[:id])
+	    @product.destroy
+	    render json: {message: "Product successfully destroyed"}
+	  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-  	get "/all_products_url" => "products#all_products_method"
-  	get "/first_product_url" => "products#first_product_method"
-  	get "/second_product_url" => "products#second_product_method"
-  	# query parameter
-  	get "/one_product" => "products#one_product"
-  	# url segment parameter
-  	get "/one_product/:id" => "products#one_product"
+  	get "/products" => 'products#index'
+    post "/products" => 'products#create'
+    get "/products/:id" => 'products#show'
+    patch "/products/:id" => 'products#update'
+    delete "/products/:id" => 'products#destroy'
   end
 end
