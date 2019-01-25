84f2fb3ced054531143f28152281a8a28864cdf8
dani <dani.zaghian@gmail.com>
2019-01-25 13:25:53 -0800

    adds supplier and products model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,11 @@ class Product < ApplicationRecord
 
 	validates :name, presence: true, uniqueness: true
 	validates :price, numericality: { greater_than: 0 }
-	validates :description, length: { in: 20..500 }
+	# validates :description, length: { in: 20..500 }
+
+	def supplier
+		Supplier.find_by(id: supplier_id) #returns a single supplier hash
+	end
 
 	def is_discounted?
 		price <= 10
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,7 @@
 class Supplier < ApplicationRecord
+
+	def products
+		Product.where(supplier_id: id) #array of products - hashes
+	end
+
 end
