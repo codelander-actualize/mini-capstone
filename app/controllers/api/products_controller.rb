3391a5a5dcfe54b6ff0a3da80a2ba2ec8a354bc1
dani <dani.zaghian@gmail.com>
2019-01-25 09:51:21 -0800

    adds active record queries to index action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,52 +1,76 @@
 class Api::ProductsController < ApplicationController
 
 	def index
-	    @products = Product.all
-	    render 'index.json.jbuilder'
-	  end
+    @products = Product.all
 
-	  def create
-	    @product = Product.new(
-	     	name: params[:name],
-	     	price: params[:price],
-	     	description: params[:description],
-	     	image_url: params[:image_url]
-	    )
-	    if @product.save
-	    	# happy path
-	    	render 'show.json.jbuilder'
-	    else
-	    	# sad path
-	    	render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
-	    end
-	  end
+    search_term = params[:search]
+    if search_term
+    	@products = Product.where("name LIKE ?", "%#{search_term}%")
+    end
 
-	  def show
-	    @product = Product.find(params[:id])
-	    render 'show.json.jbuilder'
-	  end
+  	discount = params[:discount]
+  	if discount
+  		@products = Product.where("price < ?", 10)
+  	end
 
-	  def update
-	    @product = Product.find(params[:id])
-	    
-	    @product.name = params[:name] || @product.name
-	    @product.price = params[:price] || @product.price
-	    @product.description = params[:description] || @product.description
-	    @product.image_url = params[:image_url] || @product.image_url
+    sort_term = params[:sort]
+    sort_order = params[:sort_order]
+    
+    if sort_term == "price"
+    	if sort_order == "desc"
+    		@products = @products.order(price: :desc)
+    	else
+    		@products = @products.order(:price)
+    	end
+    else
+    	@products = @products.order(:id)
+    end
 
-	    if @product.save
-	    	# happy path
-	    	render 'show.json.jbuilder'
-	    else
-	    	# sad path
-	    	render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
-	    end
-	  end
+    render 'index.json.jbuilder'
+  end
 
-	  def destroy
-	    @product = Product.find(params[:id])
-	    @product.destroy
-	    render json: {message: "Product successfully destroyed"}
-	  end
+  def create
+    @product = Product.new(
+     	name: params[:name],
+     	price: params[:price],
+     	description: params[:description],
+     	image_url: params[:image_url]
+    )
+    if @product.save
+    	# happy path
+    	render 'show.json.jbuilder'
+    else
+    	# sad path
+    	render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+
+    if @product.save
+    	# happy path
+    	render 'show.json.jbuilder'
+    else
+    	# sad path
+    	render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: {message: "Product successfully destroyed"}
+  end
 
 end
